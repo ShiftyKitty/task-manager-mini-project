ed23e42c96fed346970ff8d6230b94fa412237d3
ShiftyKitty <cathalpmcgahan@gmail.com>
2021-07-17 10:36:41 +0000

    Add app.py

--- app.py
+++ app.py
@@ -6,9 +6,11 @@ if os.path.exists("env.py"):
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
     return "Hello World again baby boy"
+
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
