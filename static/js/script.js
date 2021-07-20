26e11d1cc418a62873718b1542440a5045de4ca9
ShiftyKitty <cathalpmcgahan@gmail.com>
2021-07-20 17:29:58 +0000

    Bind data from DB to accordion

--- static/js/script.js
+++ static/js/script.js
@@ -5,4 +5,8 @@ $(document).ready(function(){
   $(document).ready(function(){
     $('.collapsible').collapsible();
   });
+
+  $(document).ready(function(){
+    $('.tooltipped').tooltip();
+  });
         
